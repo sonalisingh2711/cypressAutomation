dc2530e2b379a3511d363fc5ff39c21fbd54a311
sonalisingh2711 <singhsonali095@gmail.com>
2021-05-11 17:24:58 +0530

    updated page object model

--- cypress/integration/interactingelements.js
+++ cypress/integration/interactingelements.js
@@ -1,15 +1,21 @@
 import Home from './pageObject/home'
+import Shoes from './pageObject/shoes'
 describe("page object model",function(){
      it("searching women shoes",function(){
          var home=new Home
+         var shoes_s=new Shoes
          home.visitLink();
          home.Shoes(".prdctfltr_ft_mens-shoes");
          cy.wait(9000)
-         home.selectShoes('ASICS GEL-Kayano 27')
+         home.selectShoes('Diadora Mythos Fast Racer 2')
           cy.url().should('include','/product')
+shoes_s.colorOfShoes("Yellow Fluo/Pink Fluo")
+         shoes_s.sizeOfShoes('8');
+         shoes_s.widthOfShoes('D');
+        shoes_s.addCart();
+        
          //navigate
-         cy.go('back')
+       //  cy.go('back')
          cy.screenshot('capturing screenshot')
         })
-
 })
---
+++ cypress/integration/pageObject/shoes.js
@@ -0,0 +1,19 @@
+class Shoes{
+    colorOfShoes(value){
+    const color=cy.get("#pa_color")
+    color.select(value)
+    }
+    sizeOfShoes(value){
+        const size=cy.get("#pa_size")
+        size.select(value)
+    }
+    widthOfShoes(value){
+        const width=cy.get("#pa_width")
+        width.select(value)
+    }
+    addCart(){
+const cart=cy.get("button.single_add_to_cart_button")
+cart.click()
+    }
+}
+export default Shoes
