fd1d1d6a4df3f0f45cc7be353aa57ca4ef35565a
sonalisingh2711 <singhsonali095@gmail.com>
2021-05-11 13:48:58 +0530

    tird commit

--- cypress/integration/pageObject/home.js
+++ cypress/integration/pageObject/home.js
@@ -7,5 +7,9 @@ class Home {
         const find = cy.get(value)
         find.click()
     }
+    selectShoes(value) {
+        const clickShoe = cy.contains(value)
+        clickShoe.click()
+    }
 }
 export default Home
