c5783eb39b6a0a915dc216511c6c85c3ddf22553
sonalisingh2711 <singhsonali095@gmail.com>
2021-05-15 22:19:02 +0530

    custom and looping

---
+++ cypress/integration/css-selector.js
@@ -0,0 +1,8 @@
+describe("use assertion", function () {
+    it("css selector", function () {
+        cy.visit("https://www.w3schools.com/cssref/css_selectors.asp")
+        cy.pause()
+        cy.get("#leftmenuinnerinner >a").each($a => {
+            expect($a).to.have.attr('href')})
+    })
+})
